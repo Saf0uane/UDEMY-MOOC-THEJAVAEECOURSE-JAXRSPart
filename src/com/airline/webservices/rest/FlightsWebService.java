3ffcc616cdcd07aa282425de86d90513717bd82d
Saf0uane <elmardisafouane@gmail.com>
2016-08-13 13:05:00 +0100

    The 5th video of the jaxrs part of the java ee course

--- src/com/airline/webservices/rest/FlightsWebService.java
+++ src/com/airline/webservices/rest/FlightsWebService.java
@@ -1,12 +1,18 @@
 package com.airline.webservices.rest;
 
+import java.util.List;
+
 import javax.ejb.EJB;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
 
+import com.airline.models.Flight;
 import com.airline.service.FlightService;
 
 @Path( "/flights" )
@@ -22,6 +28,16 @@ public class FlightsWebService {
     UriInfo       fUriInfo;
 
     public FlightsWebService() {
+
+    }
+
+    @GET
+    @Produces( MediaType.APPLICATION_JSON )
+    public List<Flight> getFlights() {
+
+        List<Flight> fList = fs.getFlights();
+
+        return fList;
 
     }
 
--- src/com/airline/webservices/rest/PassengersWebService.java
+++ src/com/airline/webservices/rest/PassengersWebService.java
@@ -1,12 +1,18 @@
 package com.airline.webservices.rest;
 
+import java.util.List;
+
 import javax.ejb.EJB;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
 
+import com.airline.models.Passenger;
 import com.airline.service.PassengerService;
 
 @Path( "/passengers" )
@@ -22,6 +28,16 @@ public class PassengersWebService {
     UriInfo          pUriInfo;
 
     public PassengersWebService() {
+
+    }
+
+    @GET
+    @Produces( MediaType.APPLICATION_XML )
+    public List<Passenger> getPassengers() {
+
+        List<Passenger> pList = ps.getPassengers();
+
+        return pList;
 
     }
 
